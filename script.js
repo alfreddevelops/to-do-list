3962415b16bf2c645c80651f68b4ac5e66e41ca7
alfreddevelops <narutoalf@yahoo.com>
2020-12-24 15:37:50 +0800

    bugs: ol numbers not appearing

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$(function () {
+  // counter();
+})
+
+// function counter() {
+//   var i;
+//   for (i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// }
+
 function newItem() {
   // console.log("item added");
   var li = document.createElement("li");
@@ -5,24 +16,31 @@ function newItem() {
   var text = document.createTextNode(item);
 
   li.appendChild(text);
+
+  var n;
+  for (n = 0; n < itemList + 1; n++) {
+    console.log(n);
+  }
+
   if (!item) {
     alert('Please key in an item');
     console.log("no item added")
   } else {
     document.getElementById("itemList").appendChild(li);
     console.log(`${item} added to list`);
+
     function clearInput() {
       document.getElementById('myInput').value == "";
+      counter();
     }
   }
 }
 
-var myList = document.querySelector("ul");
-myList.addEventListener('click', function(ev) {
+var myList = document.querySelector("ol");
+myList.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('done');
     console.log('item deleted');
-    // console.log(`${text} deleted`);
   }
 }, false);
 
